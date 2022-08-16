f014ec4def80b9e9601199ec9a34275f38afabfb
Shadow King <105120735+MasterShad0w@users.noreply.github.com>
2022-08-16 16:11:14 +0530

    Update muting.py

--- tg_bot/modules/muting.py
+++ tg_bot/modules/muting.py
@@ -182,6 +182,8 @@ __help__ = """
  - /mute <userhandle>: silences a user. Can also be used as a reply, muting the replied to user.
  - /tmute <userhandle> x(m/h/d): mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
  - /unmute <userhandle>: unmutes a user. Can also be used as a reply, muting the replied to user.
+
+Created by~❤ @ShadowsArena.
 """
 
 __mod_name__ = "Muting"
